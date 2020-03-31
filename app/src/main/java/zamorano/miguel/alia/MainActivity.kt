f043150528599a4f258d88e4687a07d0c4049448
Julio Contreras <julio.contreras@potros.itson.edu.mx>
2020-03-30 19:02:51 -0700

    se agregaron los iconos y fondos. Se implementaron en sus respectivas pantallas

--- app/src/main/java/zamorano/miguel/alia/MainActivity.kt
+++ app/src/main/java/zamorano/miguel/alia/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.os.Handler;
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val boton: Button = findViewById(R.id.btn_registrar);
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent: Intent = Intent(this,registerActivity::class.java);
+            startActivity(intent);
+        }, 3000)
+
         boton.setOnClickListener({
             val intent: Intent = Intent(this,registerActivity::class.java);
             startActivity(intent);
         });
     }
+
 }
--- app/src/main/java/zamorano/miguel/alia/registerActivity.kt
+++ app/src/main/java/zamorano/miguel/alia/registerActivity.kt
@@ -1,12 +1,23 @@
 package zamorano.miguel.alia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 
 class registerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val boton: Button = findViewById(R.id.btn_listo);
+
+        boton.setOnClickListener({
+            val intent: Intent = Intent(this,MenuViaje::class.java);
+            startActivity(intent);
+        });
+
     }
 }
