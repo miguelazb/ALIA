89621fe40b9a4ffb0df7f7d76acd4469553f313f
Jose Rey <jose_rrb_chivas@hotmail.com>
2020-04-01 19:34:04 -0700

    asdas

---
+++ app/src/main/java/zamorano/miguel/alia/ViajeResume.kt
@@ -0,0 +1,12 @@
+package zamorano.miguel.alia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViajeResume : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viaje_resume)
+    }
+}
