018fcc8d364a4e82caad9b5f7f90f8cd76f0f98a
Julio Contreras <julio.contreras@potros.itson.edu.mx>
2020-04-01 19:52:24 -0700

    pantalla elige conductor e iconos seleccion agregados

---
+++ app/src/main/java/zamorano/miguel/alia/EligeConductorActivity.kt
@@ -0,0 +1,12 @@
+package zamorano.miguel.alia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EligeConductorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elige_conductor)
+    }
+}
